8e86e9d3cddf5e8521969a7f0f5df5ddddf5c48c
Mohammad Bayat <OkBayat@gmail.com>
2024-11-04 22:44:25 +0330

    Update my_plugin.rb

--- _plugins/my_plugin.rb
+++ _plugins/my_plugin.rb
@@ -49,7 +49,7 @@ end
       audio_data.each do |key, value|
         include_tag += " #{key}=&#39;#{value}&#39;"
       end
-      include_tag += " %}"
+      include_tag += " %} <p>"
 
       include_tag
     end
