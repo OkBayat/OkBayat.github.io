4d6fc6cda38517a9c169e8363abd9b87fee748db
Mohammad Bayat <OkBayat@gmail.com>
2024-11-04 18:59:00 +0330

    Update audio component

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,109 +1,125 @@
-const audio = document.getElementById("audio")
-const playPauseBtn = document.getElementById("play-pause")
-const currentTimeEl = document.getElementById("current-time")
-const durationEl = document.getElementById("duration")
-const progressContainer = document.getElementById("progress-container")
-const progress = document.getElementById("progress")
-const progressThumb = document.getElementById("progress-thumb")
-const volumeContainer = document.querySelector(".volume-container")
-const volumeProgress = document.getElementById("volume-progress")
-const volumeThumb = document.getElementById("volume-thumb")
-const muteUnmuteBtn = document.getElementById("mute-unmute")
-let isDraggingProgress = false
-let isDraggingVolume = false
+window.onload = function () {
+  const audio = document.getElementById("audio")
+  const playPauseBtn = document.getElementById("play-pause")
+  const currentTimeEl = document.getElementById("current-time")
+  const durationEl = document.getElementById("duration")
+  const progressContainer = document.getElementById("progress-container")
+  const progress = document.getElementById("progress")
+  const progressThumb = document.getElementById("progress-thumb")
+  const volumeContainer = document.querySelector(".volume-container")
+  const volumeProgress = document.getElementById("volume-progress")
+  const volumeThumb = document.getElementById("volume-thumb")
+  const muteUnmuteBtn = document.getElementById("mute-unmute")
+  let isDraggingProgress = false
+  let isDraggingVolume = false
 
-playPauseBtn.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.play()
-    playPauseBtn.classList.remove("paused")
-    playPauseBtn.classList.add("playing")
-  } else {
-    audio.pause()
-    playPauseBtn.classList.remove("playing")
-    playPauseBtn.classList.add("paused")
+  playPauseBtn.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.play()
+      playPauseBtn.classList.remove("paused")
+      playPauseBtn.classList.add("playing")
+    } else {
+      audio.pause()
+      playPauseBtn.classList.remove("playing")
+      playPauseBtn.classList.add("paused")
+    }
+  })
+
+  audio.addEventListener("timeupdate", () => {
+    if (!isDraggingProgress) {
+      const currentTime = formatTime(audio.currentTime)
+      currentTimeEl.textContent = currentTime
+
+      const progressPercent = (audio.currentTime / audio.duration) * 100
+      progress.style.width = `${progressPercent}%`
+      progressThumb.style.left = `${progressPercent}%`
+    }
+  })
+
+  audio.addEventListener("loadedmetadata", () => {
+    durationEl.textContent = formatTime(audio.duration)
+  })
+
+  function addEventListenersForDrag(element, updateFunction, setDraggingFlag) {
+    element.addEventListener("mousedown", (e) => {
+      setDraggingFlag(true)
+      updateFunction(e)
+      window.addEventListener("mousemove", updateFunction)
+      window.addEventListener("mouseup", () => {
+        setDraggingFlag(false)
+        window.removeEventListener("mousemove", updateFunction)
+      })
+    })
+
+    element.addEventListener("touchstart", (e) => {
+      setDraggingFlag(true)
+      updateFunction(e.touches[0])
+      window.addEventListener("touchmove", (event) =>
+        updateFunction(event.touches[0])
+      )
+      window.addEventListener("touchend", () => {
+        setDraggingFlag(false)
+        window.removeEventListener("touchmove", (event) =>
+          updateFunction(event.touches[0])
+        )
+      })
+    })
   }
-})
 
-audio.addEventListener("timeupdate", () => {
-  if (!isDraggingProgress) {
-    const currentTime = formatTime(audio.currentTime)
-    currentTimeEl.textContent = currentTime
+  addEventListenersForDrag(
+    progressContainer,
+    updateProgress,
+    (flag) => (isDraggingProgress = flag)
+  )
+  addEventListenersForDrag(
+    volumeContainer,
+    updateVolume,
+    (flag) => (isDraggingVolume = flag)
+  )
 
-    const progressPercent = (audio.currentTime / audio.duration) * 100
+  function updateProgress(e) {
+    const rect = progressContainer.getBoundingClientRect()
+    let offsetX = e.clientX - rect.left
+    offsetX = Math.max(0, Math.min(offsetX, rect.width))
+    const duration = audio.duration
+    const newTime = (offsetX / rect.width) * duration
+
+    if (!isDraggingVolume) {
+      audio.currentTime = newTime
+    }
+    const progressPercent = (newTime / duration) * 100
     progress.style.width = `${progressPercent}%`
     progressThumb.style.left = `${progressPercent}%`
   }
-})
 
-audio.addEventListener("loadedmetadata", () => {
-  durationEl.textContent = formatTime(audio.duration)
-})
+  function updateVolume(e) {
+    const rect = volumeContainer.getBoundingClientRect()
+    let offsetX = e.clientX - rect.left
+    offsetX = Math.max(0, Math.min(offsetX, rect.width))
+    const volume = offsetX / rect.width
 
-function addEventListenersForDrag(element, updateFunction, setDraggingFlag) {
-  element.addEventListener("mousedown", (e) => {
-    setDraggingFlag(true)
-    updateFunction(e)
-    window.addEventListener("mousemove", updateFunction)
-    window.addEventListener("mouseup", () => {
-      setDraggingFlag(false)
-      window.removeEventListener("mousemove", updateFunction)
-    })
+    audio.volume = volume
+    volumeProgress.style.width = `${volume * 100}%`
+    volumeThumb.style.left = `${volume * 100}%`
+  }
+
+  muteUnmuteBtn.addEventListener("click", () => {
+    if (audio.muted) {
+      audio.muted = false
+      muteUnmuteBtn.classList.remove("muted")
+      muteUnmuteBtn.classList.add("unmuted")
+    } else {
+      audio.muted = true
+      muteUnmuteBtn.classList.remove("unmuted")
+      muteUnmuteBtn.classList.add("muted")
+    }
   })
 
-  element.addEventListener("touchstart", (e) => {
-    setDraggingFlag(true)
-    updateFunction(e.touches[0])
-    window.addEventListener("touchmove", (event) => updateFunction(event.touches[0]))
-    window.addEventListener("touchend", () => {
-      setDraggingFlag(false)
-      window.removeEventListener("touchmove", (event) => updateFunction(event.touches[0]))
-    })
-  })
-}
-
-addEventListenersForDrag(progressContainer, updateProgress, (flag) => isDraggingProgress = flag)
-addEventListenersForDrag(volumeContainer, updateVolume, (flag) => isDraggingVolume = flag)
-
-function updateProgress(e) {
-  const rect = progressContainer.getBoundingClientRect()
-  let offsetX = e.clientX - rect.left
-  offsetX = Math.max(0, Math.min(offsetX, rect.width))
-  const duration = audio.duration
-  const newTime = (offsetX / rect.width) * duration
-
-  if (!isDraggingVolume) {
-    audio.currentTime = newTime
+  function formatTime(time) {
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+    return `${minutes < 10 ? "0" : ""}${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`
   }
-  const progressPercent = (newTime / duration) * 100
-  progress.style.width = `${progressPercent}%`
-  progressThumb.style.left = `${progressPercent}%`
-}
-
-function updateVolume(e) {
-  const rect = volumeContainer.getBoundingClientRect()
-  let offsetX = e.clientX - rect.left
-  offsetX = Math.max(0, Math.min(offsetX, rect.width))
-  const volume = offsetX / rect.width
-
-  audio.volume = volume
-  volumeProgress.style.width = `${volume * 100}%`
-  volumeThumb.style.left = `${volume * 100}%`
-}
-
-muteUnmuteBtn.addEventListener("click", () => {
-  if (audio.muted) {
-    audio.muted = false
-    muteUnmuteBtn.classList.remove("muted")
-    muteUnmuteBtn.classList.add("unmuted")
-  } else {
-    audio.muted = true
-    muteUnmuteBtn.classList.remove("unmuted")
-    muteUnmuteBtn.classList.add("muted")
-  }
-})
-
-function formatTime(time) {
-  const minutes = Math.floor(time / 60)
-  const seconds = Math.floor(time % 60)
-  return `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
 }
