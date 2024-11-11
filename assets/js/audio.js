80c1f50378a3f2c4c4c8627fb2c610e35377ed87
Mohammad Bayat <OkBayat@gmail.com>
2024-11-11 14:21:57 +0330

    Update audio.js

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,4 +1,4 @@
-function (audioId) {
+function audioPalyer(audioId) {
   const audio = document.getElementById("audio-" + audioId)
   const playPauseBtn = document.getElementById("play-pause-" + audioId)
   const currentTimeEl = document.getElementById("current-time-" + audioId)
@@ -6,7 +6,7 @@ function (audioId) {
   const progressContainer = document.getElementById("progress-container-" + audioId)
   const progress = document.getElementById("progress-" + audioId)
   const progressThumb = document.getElementById("progress-thumb-" + audioId)
-  const volumeContainer = document.querySelector(".volume-container-" + audioId)
+  const volumeContainer = document.getElementById(".volume-container-" + audioId)
   const volumeProgress = document.getElementById("volume-progress-" + audioId)
   const volumeThumb = document.getElementById("volume-thumb-" + audioId)
   const muteUnmuteBtn = document.getElementById("mute-unmute-" + audioId)
